a22157d5f089df625c4d2338db1c6cb96230d7fd
panna001 <sonar.pocket.0926@gmail.com>
2021-03-06 10:38:35 +0000

    [Add]ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
-  
+  root "homes#top"
+  get "/about" => "homes#about"
   devise_for :users
+  resources :users, only: [:show, :edit, :update] do
+    resources :relationships, only: [:create, :destroy]
+    get "/followings" => "relationships#followings", as: "followings"
+    get "/followers" => "relationships#followers", as: "followers"
+  end
+  resources :rounds, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+    resources :comments, only: [:new, :create, :edit, :update, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+  resources :notifications, only: [:index]
+  delete "/notifications" => "notifications#destroy_all", as: "destroy_all_notifications"
+  resources :chats, only: [:show, :create]
 end
