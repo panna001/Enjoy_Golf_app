353d239afd0432847aed2dd0475f51df8f94a7aa
panna001 <sonar.pocket.0926@gmail.com>
2021-03-06 09:55:13 +0000

    [Add]controller追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   root :to => "homes#top"
-  get "home/about" => "homes#about"
-
-  resources :books, only: [:index, :show, :edit, :create, :destroy, :update]
-  resources :users, only: [:index, :show, :edit, :update]
-
-  get 'chat/:id' => 'chats#show', as: 'chat'
-  resources :chats, only: [:create]
+  
+  devise_for :users
 end
