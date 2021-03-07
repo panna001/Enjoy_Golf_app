13075b23248e676dce75b15187a5987028132fe2
panna001 <sonar.pocket.0926@gmail.com>
2021-03-07 10:31:26 +0000

    [Add]新規投稿

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,8 +6,34 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+  end
+  
+  def create
+    post = current_user.posts.new(post_params)
+    if post.save
+      redirect_to post_path(post)
+    else
+      render :new
+    end
   end
 
   def edit
+    post = Post.find(params[:id])
+    if post = current_user.posts.update(post_params)
+      redirect_to post_path(post)
+    else
+      render :edit
+    end
   end
+  
+  def update
+    
+  end
+  
+  private
+  def post_params
+    params.require(:post).permit(:post_image, :body)
+  end
+  
 end
