dac8175381032a7a81c364306fa10e4eec24da3d
panna001 <sonar.pocket.0926@gmail.com>
2021-03-09 06:39:40 +0000

    [Add]フォローフォロワー一覧

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,12 @@ class UsersController < ApplicationController
   end
   
   def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings
   end
 
   def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers
   end
 end
