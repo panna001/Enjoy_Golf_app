df8298654021a760ce78344513af89a423a3ec5a
panna001 <sonar.pocket.0926@gmail.com>
2021-03-11 01:53:01 +0000

    [Add]ラウンド一覧大枠作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ApplicationRecord
   has_many :followings, through: :active_relationships, source: :follower
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "follower_id"
   has_many :followers, through: :passive_relationships, source: :following
+  has_many :rounds, dependent: :destroy
   
   validates :account_name, :first_name, :last_name, :sex, :prefecture, presence: true
   validates :account_name, uniqueness: true
