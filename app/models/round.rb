6a7352aad3516c7fb7287237dd24b8976d9bf5c8
panna001 <sonar.pocket.0926@gmail.com>
2021-03-11 03:43:18 +0000

    [Add]round新規投稿画面作成途中

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,5 +1,7 @@
 class Round < ApplicationRecord
   belongs_to :user
+  has_many :scores, dependent: :destroy
+  accepts_nested_attributes_for :scores
   
   validates :date, :place, presence: true
 end
