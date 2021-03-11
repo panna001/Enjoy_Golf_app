7c2346b9f7e19261177c6346de0e2006418b3fb3
panna001 <sonar.pocket.0926@gmail.com>
2021-03-11 03:43:18 +0000

    [Add]round新規投稿画面作成途中

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -10,8 +10,28 @@ class RoundsController < ApplicationController
   end
 
   def new
+    @user = current_user
+    @round = Round.new
+    @round.scores.build
+  end
+  
+  def create
+    @user = current_user
+    @round = current_user.rounds.build(round_params)
+    @round.save
+    # if @round.save
+      redirect_to user_round_path(user_id: current_user, id: @round.id)
+    # else
+    #   render :new
+    # end
   end
 
   def edit
   end
+  
+  private
+  def round_params
+    params.require(:round).permit(:date, :place, :weather, :wind, scores_attributes: [:hole_number, :par_count, :stroke_count, :put_count, :fairway_keep, :ob_count, :penalty_count, :par_on])
+  end
+  
 end
