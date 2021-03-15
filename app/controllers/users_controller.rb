49970124eb9ecd7b531c745bb1747a1ba008db19
panna001 <sonar.pocket.0926@gmail.com>
2021-03-15 06:06:46 +0000

    [Add]ユーザー詳細のスコア表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  
+
   def show
     @user = User.find(params[:id])
+    on_numbers = @user.scores.order(id: :DESC).limit(90).map{|s|s.par_count - (s.stroke_count - s.putt_count)}
+    @score = (@user.scores.order(id: :DESC).limit(5).sum(:stroke_count) / @user.rounds.limit(5).count.to_f).round(1)
+    @putt = (@user.scores.order(id: :DESC).limit(5).sum(:putt_count) / @user.rounds.limit(5).count.to_f).round(1)
+    @fairway_keep_rate = (@user.scores.order(id: :DESC).limit(90).where(fairway_keep: "○").count / @user.scores.limit(90).count.to_f * 100).round(1)
+    @par_on_rate = (on_numbers.count(2) / @user.scores.limit(90).count.to_f * 100).round(1)
+    @under_par_on_rate = (on_numbers.count(3) / @user.scores.limit(90).count.to_f * 100).round(1)
   end
 
   def edit
@@ -11,16 +17,16 @@ class UsersController < ApplicationController
       redirect_back(fallback_location: root_path)
     end
   end
-  
+
   def update
     user = User.find(params[:id])
     if user.update(user_params)
       redirect_to user_path(user)
-    else 
+    else
       render :edit
     end
   end
-  
+
   def followings
     @user = User.find(params[:id])
     @followings = @user.followings
@@ -30,7 +36,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @followers = @user.followers
   end
-  
+
   private
   def user_params
     params.require(:user).permit(:account_name, :first_name, :last_name, :introduction, :sex, :email, :prefecture, :start_year, :start_month, :profile_image)
