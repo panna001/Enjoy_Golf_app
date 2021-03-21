6fb1341cc9259f2e13096268826588d7038d0931
panna001 <75406423+panna001@users.noreply.github.com>
2021-03-21 12:49:37 +0900

    Chat (#35)
    * [Add]chat機能
    
    * [Add]非同期化完了

--- db/schema.rb
+++ db/schema.rb
@@ -20,9 +20,13 @@ ActiveRecord::Schema.define(version: 2021_03_08_120352) do
   end
 
   create_table "chats", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "room_id"
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_chats_on_room_id"
+    t.index ["user_id"], name: "index_chats_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -64,9 +68,7 @@ ActiveRecord::Schema.define(version: 2021_03_08_120352) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
-  end
-
-  create_table "relationships", force: :cascade do |t|
+  endonships", force: :cascade do |t|
     t.integer "following_id"
     t.integer "follower_id"
     t.datetime "created_at", null: false
