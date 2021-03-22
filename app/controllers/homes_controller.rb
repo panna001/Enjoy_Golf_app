dc1b4b4754e94f6e4ed133b9b60ef8f1894b37d3
panna001 <sonar.pocket.0926@gmail.com>
2021-03-22 10:16:02 +0000

    [Add]aboutページの新着＆人気投稿表示

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,5 +3,8 @@ class HomesController < ApplicationController
   end
 
   def about
+    @new_posts = Post.all.order(id: :desc).first(3)
+
+    @favorite_posts = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(3).pluck(:post_id))
   end
 end
