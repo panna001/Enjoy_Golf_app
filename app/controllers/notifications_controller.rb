012574454f94d0512da692b4dc961d91f0d27488
panna001 <75406423+panna001@users.noreply.github.com>
2021-03-25 16:53:28 +0900

    Fix function (#44)
    * [Update]ヘッダー調整
    
    * [Fixed]投稿編集リンク
    
    * [Update]全体の色更新
    
    * [Update]投稿一覧をフォローユーザーのみに変更
    
    * [Add]おすすめ機能実装
    
    * [Update]フォローを促す通知追加
    
    * [Update]投稿検索
    
    * [Add]bookmark画面
    
    * [Update]コメント表示修正
    
    * [Update]コメント欄作成
    
    * [Update]postのコメント非同期完了
    
    * [Update]コメント非同期完了
    
    * [Update]コメントフォームのtoggle化
    
    * [Update]続きを読むUpdate
    
    * [Update]コメント開閉
    
    * [Update]postフォローボタン非同期
    
    * [Update]フォロー一覧フォロワー一覧のボタン非同期
    
    * [Update]ユーザー詳細レスポンシブ
    
    * [Update]通知機能のみため調整
    
    * [Update]パジネーション
    
    * [Update]フォローボタンの自分へのリンク非表示
    
    * [Fixed]投稿削除時のエラー修正

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,9 +2,10 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @notifications = current_user.passive_notifications.page(params[:page]).per(20)
-    @notifications.where(checked: false).each do |notification|
+    notifications = current_user.passive_notifications
+    notifications.where(checked: false).each do |notification|
       notification.update_attributes(checked: true)
     end
+    @notifications = notifications.where.not(visitor_id: current_user.id).page(params[:page]).per(5)
   end
 end
