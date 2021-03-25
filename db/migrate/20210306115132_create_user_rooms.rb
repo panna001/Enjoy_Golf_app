7cc607601c7007a056c4be5e4aa90c3a978c6f0d
panna001 <sonar.pocket.0926@gmail.com>
2021-03-25 11:28:53 +0000

    [Update]migrate

--- db/migrate/20210306115132_create_user_rooms.rb
+++ db/migrate/20210306115132_create_user_rooms.rb
@@ -1,8 +1,8 @@
 class CreateUserRooms < ActiveRecord::Migration[5.0]
   def change
     create_table :user_rooms do |t|
-      t.references :user, foreign_key: true
-      t.references :room, foreign_key: true
+      t.integer :user_id
+      t.integer :room_id
 
       t.timestamps
     end
