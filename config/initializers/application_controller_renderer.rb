55e5154e60f689beeb79d1a010dadaf5bbb2631b
panna001 <sonar.pocket.0926@gmail.com>
2021-03-25 11:53:39 +0000

    [Add]refile secretkey

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'f250b3301569b54ade2b59fe83d201ae1673fe357cb9ba8a92a8483477c2ee2cd713931e09e53f55fecb623bf7d4a7f05a4c0f484feba59d955d40c6154619fb'
