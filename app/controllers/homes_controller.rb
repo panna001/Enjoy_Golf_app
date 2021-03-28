8b38a746d451abbdfcec6f15bc3a0cebd77f6ba3
panna001 <sonar.pocket.0926@gmail.com>
2021-03-28 15:08:48 +0000

    [Update]修正

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -5,7 +5,7 @@ class HomesController < ApplicationController
   def about
     @new_posts = Post.all.order(id: :desc).first(3)
     @favorite_posts = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(3).pluck(:post_id))
-    @ranking_men = User.where(sex: 1).order(average: :asc).first(3)
-    @ranking_women = User.where(sex: 2).order(average: :asc).first(3)
+    @ranking_men = User.where.not(average: nil).where(sex: 1).order(average: :asc).first(3)
+    @ranking_women = User.where.not(average: nil).where(sex: 2).order(average: :asc).first(3)
   end
 end
