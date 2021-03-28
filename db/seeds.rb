742b25e695929bf0288f5da2bbeee0b9d5e55697
panna001 <sonar.pocket.0926@gmail.com>
2021-03-28 09:51:13 +0000

    [Update] seed編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,91 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+# ラウンド用
+place_a = [
+  "パインレークCC",
+  "ロータリーゴルフクラブ",
+  "かずさCC",
+  "千葉新日本CC",
+  "山の原GC",
+  "大津CC",
+  "鈴鹿CC",
+  "水戸GC",
+  "奈良名阪GC",
+  "沼津国際CC",
+  "桑名国際ゴルフCC",
+  "ラ・ヴィスタゴルフリゾート",
+  "名阪チサンCC",
+  "大多喜CC"
+]
+
+weather_a = [
+  "晴れ",
+  "くもり",
+  "雨",
+  "雪"
+]
+
+wind_a = [
+  "無風",
+  "微風",
+  "強風"
+]
+
+stroke_count_s = 60..72
+stroke_count_a1 = 72..81
+stroke_count_a2 = 81..90
+stroke_count_b1 = 90..99
+stroke_count_b2 = 99..108
+stroke_count_c1 = 108..117
+stroke_count_c2 = 117..126
+stroke_count_d1 = 126..135
+stroke_count_d2 = 135..144
+stroke_count_e = 144..153
+
+putt_count_s = 20..25
+putt_count_a1 = 24..29
+putt_count_a2 = 25..33
+putt_count_b1 = 29..37
+putt_count_b2 = 33..41
+putt_count_c1 = 37..45
+putt_count_c2 = 41..49
+putt_count_d1 = 45..53
+putt_count_d2 = 49..57
+putt_count_e = 53..61
+
+par_on_count_a = 13..16
+par_on_count_b = 10..13
+par_on_count_c = 7..10
+par_on_count_d = 4..7
+par_on_count_e = 1..4
+
+fairway_count_a = 13..16
+fairway_count_b = 10..13
+fairway_count_c = 7..10
+fairway_count_d = 4..7
+fairway_count_e = 1..4
+
+ob_count_a = 0..1
+ob_count_b = 0..2
+ob_count_c = 0..3
+ob_count_d = 0..4
+ob_count_e = 0..5
+
+penalty_count_a = 0..1
+penalty_count_b = 0..2
+penalty_count_c = 0..3
+penalty_count_d = 0..4
+penalty_count_e = 0..5
+
+# ラウンド日
+from = Time.parse("2018/01/01")
+to = Time.parse("2021/03/28")
+
+
+
+
 User.create!(
   [
     {
@@ -21,7 +106,8 @@ User.create!(
       start_year: rand(2010..2020),
       start_month: rand(1..12),
       teens: "10代",
-      rank: "A-"
+      rank: "AA",
+      average: stroke_count_s
     },
     {
       account_name: "Sara",
@@ -36,7 +122,8 @@ User.create!(
       start_year: rand(2010..2020),
       start_month: rand(1..12),
       teens: "20代",
-      rank: "B+"
+      rank: "A+",
+      average: stroke_count_a1
     },
     {
       account_name: "Tom",
@@ -51,7 +138,8 @@ User.create!(
       start_year: rand(2010..2020),
       start_month: rand(1..12),
       teens: "30代",
-      rank: "D+"
+      rank: "A-",
+      average: stroke_count_a2
     },
     {
       account_name: "Tomas",
@@ -66,7 +154,8 @@ User.create!(
       start_year: rand(2010..2020),
       start_month: rand(1..12),
       teens: "20代",
-      rank: "E"
+      rank: "B+",
+      average: stroke_count_b1
     }
   ]
 )
@@ -123,180 +212,3 @@ User.find(4).posts.create!(
   ]
 )
 
-User.find(1).rounds.create!(
-  [
-    {
-      play_date: "2020/10/01",
-      place: "ローズウッド",
-      weather: "くもり",
-      wind: "無風"
-    },
-    {
-      play_date: "2020/10/02",
-      place: "阪奈CC",
-      weather: "晴れ",
-      wind: "強風"
-    }
-  ]
-)
-
-User.find(2).rounds.create!(
-  [
-    {
-      play_date: "2020/11/01",
-      place: "白鵬CC",
-      weather: "雨",
-      wind: "弱風"
-    },
-    {
-      play_date: "2020/11/02",
-      place: "高槻CC",
-      weather: "晴れ",
-      wind: "無風"
-    }
-  ]
-)
-
-User.find(3).rounds.create!(
-  [
-    {
-      play_date: "2020/12/01",
-      place: "東條CC",
-      weather: "くもり",
-      wind: "強風"
-    },
-    {
-      play_date: "2020/12/02",
-      place: "阪奈CC",
-      weather: "雪",
-      wind: "強風"
-    }
-  ]
-)
-
-User.find(4).rounds.create!(
-  [
-    {
-      play_date: "2020/01/01",
-      place: "パインレークCC",
-      weather: "晴れ",
-      wind: "無風"
-    },
-    {
-      play_date: "2020/01/02",
-      place: "東大阪CC",
-      weather: "晴れ",
-      wind: "弱風"
-    },
-    {
-      play_date: "2020/03/02",
-      place: "東大阪CC",
-      weather: "晴れ",
-      wind: "弱風"
-    },
-    {
-      play_date: "2020/04/02",
-      place: "東大阪CC",
-      weather: "晴れ",
-      wind: "弱風"
-    },
-    {
-      play_date: "2020/05/02",
-      place: "東大阪CC",
-      weather: "晴れ",
-      wind: "弱風"
-    },
-    {
-      play_date: "2020/06/01",
-      place: "東大阪CC",
-      weather: "晴れ",
-      wind: "弱風"
-    },
-    {
-      play_date: "2020/07/02",
-      place: "東大阪CC",
-      weather: "晴れ",
-      wind: "弱風"
-    },
-    {
-      play_date: "2020/08/02",
-      place: "東大阪CC",
-      weather: "晴れ",
-      wind: "弱風"
-    },
-    {
-      play_date: "2020/09/02",
-      place: "東大阪CC",
-      weather: "晴れ",
-      wind: "弱風"
-    },
-    {
-      play_date: "2020/10/02",
-      place: "東大阪CC",
-      weather: "晴れ",
-      wind: "弱風"
-    },
-    {
-      play_date: "2020/11/01",
-      place: "東大阪CC",
-      weather: "晴れ",
-      wind: "弱風"
-    },
-  ]
-)
-
-2.times do |n|
-  18.times do |i|
-    Round.find(n + 1).scores.create!(
-      hole_number: i + 1,
-      par_count: rand(3..5),
-      stroke_count: rand(3..7),
-      putt_count: rand(0..2),
-      fairway_keep: rand(0..1),
-      ob_count: rand(0..1),
-      penalty_count: rand(0..1)
-    )
-  end
-end
-
-2.times do |n|
-  18.times do |i|
-    Round.find(n + 3).scores.create!(
-      hole_number: i + 1,
-      par_count: rand(3..5),
-      stroke_count: rand(4..7),
-      putt_count: rand(1..3),
-      fairway_keep: rand(0..1),
-      ob_count: rand(0..1),
-      penalty_count: rand(0..1)
-    )
-  end
-end
-
-2.times do |n|
-  18.times do |i|
-    Round.find(n + 5).scores.create!(
-      hole_number: i + 1,
-      par_count: rand(3..5),
-      stroke_count: rand(6..9),
-      putt_count: rand(1..3),
-      fairway_keep: rand(0..1),
-      ob_count: rand(0..1),
-      penalty_count: rand(0..1)
-    )
-  end
-end
-
-11.times do |n|
-  18.times do |i|
-    Round.find(n + 7).scores.create!(
-      hole_number: i + 1,
-      par_count: rand(3..5),
-      stroke_count: rand(7..10),
-      putt_count: rand(1..4),
-      fairway_keep: rand(0..1),
-      ob_count: rand(0..1),
-      penalty_count: rand(0..1)
-    )
-  end
-end
