d1d173cd71bc93242e4d06a9ec5c7f4846551691
panna001 <sonar.pocket.0926@gmail.com>
2021-04-04 03:41:55 +0000

    [Add] vision.rb

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module EnjoyGolf
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.0
     config.time_zone = 'Asia/Tokyo'
+    config.paths.add 'lib', eager_load: true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
